b3a2d036f3518aa75f7628feac8f350f71cfbb9d
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-04-26 15:37:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState, Fragment, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
-import { Album } from "./album";
 import Expense from "./expense";
 import Login from "../Login";
 import {
